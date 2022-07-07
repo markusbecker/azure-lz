3ae0bc68e5c2f7858213eed70912a6906b6ba95e
Markus Becker <markus.becker@cloud-plant.com>
2022-07-07 22:16:39 +0200

    First test

---
+++ backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "testrg"
+    storage_account_name = "cptest"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+    use_oidc             = true
+    subscription_id      = "1582aa55-9908-44bc-8cf0-5d04ada72717"
+    tenant_id            = "b1ec7715-6fd7-4e7f-aa65-85797d2c6d21"
+  }
+}
---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "tftestrg"
+  location = "ger-west-central"
+}
