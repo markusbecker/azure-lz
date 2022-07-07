fdacd30c542a7abba348006699eb1512994fe012
Markus Becker <markus.becker@outlook.com>
2022-07-07 23:26:18 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,7 +5,7 @@ terraform {
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
     use_oidc             = true
-    #subscription_id      = "1582aa55-9908-44bc-8cf0-5d04ada72717"
+    ##subscription_id      = "1582aa55-9908-44bc-8cf0-5d04ada72717"
     #tenant_id            = "b1ec7715-6fd7-4e7f-aa65-85797d2c6d21"
   }
 }
